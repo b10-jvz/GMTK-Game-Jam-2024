54ed08ca4aca5989b2a952370a558aa442a9432a
Yasen Dinkov <yd@blue10.com>
2024-08-19 22:32:19 +0200

    Fixed Bugs

--- gekkocrescsendo/assets/trees/tree.gd
+++ gekkocrescsendo/assets/trees/tree.gd
@@ -5,6 +5,7 @@ var _type = 0
 @export_enum("Type1","Type2","Type3","Type4") var BoulderType : set = _set_type, get = _get_type
 
 func _set_type(type):
+	if($Sprites == null): return
 	_type = type
 	var boulders = $Sprites.get_children()
 	for boulder in boulders:
--- gekkocrescsendo/scenes/levels/slab-level/level_goal.gd
+++ gekkocrescsendo/scenes/levels/slab-level/level_goal.gd
@@ -13,11 +13,11 @@ func _process(delta: float) -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is Boulder:
 		body.FollowNode = null
-	$Timer.start()
+		$Timer.start()
 	
 func _on_timer_timeout() -> void:
 	get_parent().game_started = false
 	$AnimationPlayer.play("end")
 
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+func _on_animation_player_animation_finished_end_game(anim_name: StringName) -> void:
 	get_tree().change_scene_to_file("res://scenes/menus/Credits.tscn")
