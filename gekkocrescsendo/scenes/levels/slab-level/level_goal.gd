dabfa6ead62b7409e63f6aea53f5ecc87326d363
Yasen Dinkov <yd@blue10.com>
2024-08-19 22:25:52 +0200

    Added trees, cuscene and end-level

---
+++ gekkocrescsendo/scenes/levels/slab-level/level_goal.gd
@@ -0,0 +1,23 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	rotate_y(deg_to_rad(-.5))
+	pass
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is Boulder:
+		body.FollowNode = null
+	$Timer.start()
+	
+func _on_timer_timeout() -> void:
+	get_parent().game_started = false
+	$AnimationPlayer.play("end")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	get_tree().change_scene_to_file("res://scenes/menus/Credits.tscn")
